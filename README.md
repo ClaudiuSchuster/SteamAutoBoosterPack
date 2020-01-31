b6cec9f5dc30491a692c3e6ac021109de143e476
Jim Chen <jim71183@gmail.com>
2020-01-31 15:48:59 +0800

    update

--- README.md
+++ README.md
@@ -3,5 +3,21 @@ No need to create and unpack steam booster pack manually everyday.
 
 For using this script, [![steam 0.9.1](https://img.shields.io/badge/steam-0.9.1-blue.svg)](https://pypi.org/project/steam/) package must be installed for python3.
 
-`unpack_url` must be changed for diffent steam account, for my inventory path, it is `Chihang0711`.
-`game_id` is a list containing all the games which you want to make booster pack.
+There are 4 variable are customized :
+
+`username` : string, your steam account
+
+`password` : string, your steam password
+
+`inventory_id` : string, your inventory id, you can go to the inventory page on steam to check this.
+
+`game_id` : list, app id of steam games that you want to create steam booster pack.
+
+
+## Usage : 
+
+<p align="center">
+  <img src="img/demo.jpg" width = "180" height = "390" border="5">
+</p>
+
+python SteamMakeBoosterPack.py "JNGTT"
---
+++ SteamMakeBoosterPack.py
@@ -0,0 +1,48 @@
+import steam
+import re
+import requests
+import datetime
+import time
+import os, sys
+
+twofactor_code = sys.argv[1]
+username = 'your_steam_account'
+password = 'your_steam_password'
+inventory_id = "Chihang0711"
+game_id = ["292030","20920", "105600"]
+
+make_url = 'https://steamcommunity.com//tradingcards/ajaxcreatebooster'
+unpack_url = f'https://steamcommunity.com/id/{inventory_id}/ajaxunpackbooster/'
+headers = {
+    "Referer": "https://steamcommunity.com/tradingcards/boostercreator/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+}
+account = steam.webauth.WebAuth(username, password)
+account_session = account.login(twofactor_code=twofactor_code)
+while True:
+    for gi in game_id:
+        params = {
+            "sessionid" : account.session_id,
+            "appid": gi,
+            "series": 1,
+            "tradability_preference" : 2
+        }
+        response = account_session.post(make_url, headers=headers, data=params)
+        if response.text[2:18]!='purchase_eresult':
+            print(f"Puchase booster pack SUCCESS on game : {gi}")
+            item_id = re.search(r'"communityitemid":"([^"]+)"', response.text).group(1)
+            params = {
+                "appid": gi,
+                "communityitemid": item_id,
+                "sessionid" : account.session_id
+            }
+            account_session.post(unpack_url, headers=headers, data=params)
+        else:
+            print(f"Puchase booster pack FAILED on game : {gi}")
+            
+    print("Current time :",str(datetime.datetime.now())[:19])
+    time.sleep(60*60*24+60*5) #Retry after 24hours and 5minutes
+
+
+
+
