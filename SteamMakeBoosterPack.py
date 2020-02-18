5a8574c4b5a7828cf7d76f46d70b771011e84ad2
Jim Chen <jim71183@gmail.com>
2020-02-18 10:48:44 +0800

    display game name
    display game name

--- SteamMakeBoosterPack.py
+++ SteamMakeBoosterPack.py
@@ -4,12 +4,16 @@ import requests
 import datetime
 import time
 import os, sys
+import steamfront
 
 username = 'your_steam_account'
 password = 'your_steam_password'
 inventory_id = "Chihang0711"
 game_id = ["292030","20920", "105600"]
 
+
+
+client = steamfront.Client()
 make_url = 'https://steamcommunity.com//tradingcards/ajaxcreatebooster'
 unpack_url = f'https://steamcommunity.com/id/{inventory_id}/ajaxunpackbooster/'
 headers = {
@@ -17,9 +21,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
 }
 account = steam.webauth.WebAuth(username, password)
-
-twofactor_code = input("Please enter steam guard code : ")
-account_session = account.login(twofactor_code=twofactor_code)
+twofactor_code_inp = input("Please enter steam guard code : ")
+account_session = account.login(twofactor_code=twofactor_code_inp)
 while True:
     for gi in game_id:
         params = {
@@ -30,7 +33,7 @@ while True:
         }
         response = account_session.post(make_url, headers=headers, data=params)
         if response.text[2:18]!='purchase_eresult':
-            print(f"Puchase booster pack SUCCESS on game : {gi}")
+            print(f"Puchase booster pack SUCCESS on game : {client.getApp(appid=gi).name}-{gi}")
             item_id = re.search(r'"communityitemid":"([^"]+)"', response.text).group(1)
             params = {
                 "appid": gi,
@@ -39,10 +42,13 @@ while True:
             }
             account_session.post(unpack_url, headers=headers, data=params)
         else:
-            print(f"Puchase booster pack FAILED on game : {gi}")
+            print(f"Puchase booster pack FAILED on game : {client.getApp(appid=gi).name}-{gi}")
             
     print("Current time :",str(datetime.datetime.now())[:19])
-    time.sleep(60*60*24+60*5) #Retry after 24hours and 5minutes
+    time.sleep(60*60*6+60) #Retry after 4 hours and 1 minutes
+
+
+# In[4]:
 
 
 
