8cca33d20202dd9884d901d74e3a4c7128c87287
ChiHangChen <jim71183@gmail.com>
2020-03-16 23:11:06 +0800

    Update SteamMakeBoosterPack.py

--- SteamMakeBoosterPack.py
+++ SteamMakeBoosterPack.py
@@ -12,7 +12,6 @@ inventory_id = "Chihang0711"
 game_id = ["292030","20920", "105600"]
 
 
-
 client = steamfront.Client()
 make_url = 'https://steamcommunity.com//tradingcards/ajaxcreatebooster'
 unpack_url = f'https://steamcommunity.com/id/{inventory_id}/ajaxunpackbooster/'
@@ -23,15 +22,19 @@ headers = {
 account = steam.webauth.WebAuth(username, password)
 twofactor_code_inp = input("Please enter steam guard code : ")
 account_session = account.login(twofactor_code=twofactor_code_inp)
+
 while True:
     for gi in game_id:
         params = {
             "sessionid" : account.session_id,
             "appid": gi,
             "series": 1,
-            "tradability_preference" : 2
+            "tradability_preference" : 1
         }
         response = account_session.post(make_url, headers=headers, data=params)
+        if goo_amount <= 1000:
+            print("Steam gems stock low, please re-fill.")
+            break
         if response.text[2:18]!='purchase_eresult':
             print(f"Puchase booster pack SUCCESS on game : {client.getApp(appid=gi).name}-{gi}")
             item_id = re.search(r'"communityitemid":"([^"]+)"', response.text).group(1)
@@ -46,10 +49,4 @@ while True:
             
     print("Current time :",str(datetime.datetime.now())[:19])
     time.sleep(60*60*6+60) #Retry after 4 hours and 1 minutes
-
-
-# In[4]:
-
-
-
 
